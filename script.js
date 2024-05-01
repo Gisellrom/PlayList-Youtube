e986347ad83e1681d11c00c8711b55bdbd5a6e42
Gisellrom <romero.giselln@gmail.com>
2024-05-01 14:32:33 -0600

    mostrar video y lista con la API

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ $(document).ready(function(){
     const btnSearch = $("#btnSearch");
     const inputPlaylistId = $("#idPlaylist");
 
+    const videoPlayer = $("#videoFrame");
+
+    const divNotFoundInputAlert = $("#divNotFoundInputAlert");
+
 
     //ESTADO INICIAL
     divEmptyInputAlert.hide(); //Escondido
+    divNotFoundInputAlert.hide(); //escondido
 
 
     // URL del API de youtube      
@@ -38,24 +43,39 @@ $(document).ready(function(){
             console.log("estoy acá");
             // Ocultamos mensajes de alerta si hay
             $(".ID-Empty").hide(600); //esconde
-            // Invocación AJAX
-            //$.get(URL,"playlistId="+inputPlaylistId+"&maxResults=50&part=id,snippet&key="+APIKey,cargaPlaylist);
+            //Invocación AJAX
+            /* $.get(
+                URL,
+                "playlistId=" +
+                  playlistID +
+                  "&maxResults=50&part=id,snippet&key=" +
+                  APIKey,
+                function (data) {
+                  // Vemos respuesta de invocacion
+                  console.log(`datos de respuesta: ${JSON.stringify(data)}`);
+                }
+            )*/
+
+            $.get(
+                URL,
+                "playlistId="+playlistID+
+                "&maxResults=50&part=id,snippet&key="+APIKey,
+                cargaPlaylist);
         }
     });
-
-    
-    
-
     
     /*******************************************************
      * Funcion para cargar la Playlist:
      * // Callback. Playlist es un objeto que contiene otros objetos que son las entradas de la lista
      ******************************************************/
     
-    /*function cargaPlaylist(playlist)
+    function cargaPlaylist(playlist)
     {
        // Este mensaje aparece en la consola de desarrollo de JavaScript en el navegador
        console.log("Encontré lista con "+playlist.items.length+" elementos");
+
+       //Mostramos el video
+       playVideo(`${playlist.items[0].snippet.resourceId.videoId}`)
 
        // Crea una lista
        $("#contenido").append($("<ul>"));
@@ -65,13 +85,27 @@ $(document).ready(function(){
         for (i=0;i<playlist.items.length;i++) {
             var element = playlist.items[i];
 
+            //$("#contenido").append(
+            //    $(`<li> <a href='https://www.youtube.com/embed/"+element.snippet.resourceId.videoId+"+"</a></li>'>`)
+            //);
+
             $("#contenido").append($("<li> <a href='https://www.youtube.com/embed/"+element.snippet.resourceId.videoId+"'>"+element.snippet.title+"</a></li>"));
-                        
+            /*<div class='cancion'>
+            <img src="./img/Youtube_Music_icon.png" alt="iconoCancion">
+            <div class="info">
+                <h6>$()</h6>
+                <p>-`+element.snippet.channelTitle+`</p>
+            </div>
+        </div>*/
         }
         $("#contenido").append($("</ul>"));
 
-    }*/
+    }//Fin de cargarPlaylist
 
-
+    function playVideo(videoID){
+        console.log(videoID);
+        srcVideo = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+        videoPlayer.attr("src", srcVideo)
+    }//fin de playVideo
     
 });
