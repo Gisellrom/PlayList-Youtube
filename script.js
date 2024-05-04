48b1ee2a7e95494fcaeb66202cce27a2683a319a
Gisellrom <romero.giselln@gmail.com>
2024-05-03 18:45:18 -0600

    acomoda la lista de reproducción

--- script.js
+++ script.js
@@ -60,7 +60,8 @@ $(document).ready(function(){
                 URL,
                 "playlistId="+playlistID+
                 "&maxResults=50&part=id,snippet&key="+APIKey,
-                cargaPlaylist);
+                cargaPlaylist
+            )
         }
     });
     
@@ -72,40 +73,83 @@ $(document).ready(function(){
     function cargaPlaylist(playlist)
     {
        // Este mensaje aparece en la consola de desarrollo de JavaScript en el navegador
-       console.log("Encontré lista con "+playlist.items.length+" elementos");
+       //console.log("Encontré lista con "+playlist.items.length+" elementos");
 
        //Mostramos el video
        playVideo(`${playlist.items[0].snippet.resourceId.videoId}`)
 
        // Crea una lista
-       $("#contenido").append($("<ul>"));
+       $("#contenido").append($(`<ul>`));
         // Recorre los objetos de la playlist y genera la lista
         var i;
                             
         for (i=0;i<playlist.items.length;i++) {
             var element = playlist.items[i];
 
-            //$("#contenido").append(
-            //    $(`<li> <a href='https://www.youtube.com/embed/"+element.snippet.resourceId.videoId+"+"</a></li>'>`)
-            //);
+            if(element.snippet.title != "Deleted video"){
 
-            $("#contenido").append($("<li> <a href='https://www.youtube.com/embed/"+element.snippet.resourceId.videoId+"'>"+element.snippet.title+"</a></li>"));
-            /*<div class='cancion'>
-            <img src="./img/Youtube_Music_icon.png" alt="iconoCancion">
-            <div class="info">
-                <h6>$()</h6>
-                <p>-`+element.snippet.channelTitle+`</p>
-            </div>
-        </div>*/
+                //$("#contenido").append(
+                //    $(`<li><a href="https://www.youtube.com/embed/"${element.snippet.resourceId.videoId}>${element.snippet.title}</a></li>`)
+                //);
+                //console.log(element.snippet.resourceId.videoId);
+            
+                /*/$("#contenido").append(
+                    $(`<li onclick="playVideo('${element.snippet.resourceId.videoId}')" style="list-style-type: none">
+                    <a>
+                    <div class='cancion'>
+                        <div class="div-img">
+                            <img src="${element.snippet.thumbnails.default.url}" width=${element.snippet.thumbnails.default.width} height=${element.snippet.thumbnails.default.height}>
+                        </div>
+                        
+                        <div class="info">
+                            <h6>${element.snippet.title}</h6>
+                            <p>${element.snippet.channelTitle}</p>
+                        </div>
+                    </div>
+                    </a>
+                    </li>`)
+                );*/
+
+                $("#contenido").append(
+                    $("<li>").on("click", function() {
+                        playVideo(element.snippet.resourceId.videoId);
+                    }).css("list-style-type", "none").append(
+                        $("<a>").append(
+                            $("<div>").addClass("cancion").append(
+                                $("<div>").addClass("div-img").append(
+                                    $("<img>").attr({
+                                        src: element.snippet.thumbnails.default.url,
+                                        width: element.snippet.thumbnails.default.width,
+                                        height: element.snippet.thumbnails.default.height
+                                    })
+                                ),
+                                $("<div>").addClass("info").append(
+                                    $("<h6>").text(element.snippet.title),
+                                    $("<p>").text(element.snippet.channelTitle)
+                                )
+                            )
+                        )
+                    )
+                );                
+            }//fin del if
         }
         $("#contenido").append($("</ul>"));
 
     }//Fin de cargarPlaylist
 
-    function playVideo(videoID){
-        console.log(videoID);
-        srcVideo = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
-        videoPlayer.attr("src", srcVideo)
+    /*function playVideo(videoId){
+        console.log(`playvideo= ${videoId}`);
+        srcVideo = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        videoPlayer.attr("src", srcVideo);
+        videoPlayer.show(1000);
     }//fin de playVideo
-    
+    */
+
+    function playVideo(videoId) {
+        console.log(`playvideo= ${videoId}`);
+        srcVideo = `https://www.youtube.com/embed/${videoId}?audioplay=1`;
+        videoPlayer.attr("src", srcVideo);
+        videoPlayer.show(1000);
+
+    } 
 });
